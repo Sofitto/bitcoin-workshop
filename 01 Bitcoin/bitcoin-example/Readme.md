a951d7a8a587061066f9505f48c55782e82592c3
Igor <igor.ruzanov@gmail.com>
2018-09-24 18:03:13 +0200

    restructure

--- 01 Bitcoin/bitcoin-example/Readme.md
+++
@@ -1,7 +0,0 @@
-Howto:
-
-```text
-$ npm install
-$ node app.js
-```
-Open in browser http://localhost:3030/
--- 01 Bitcoin/bitcoin-example/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use('/', express.static(__dirname + '/static'));
-
-var port = 3030;
-app.listen(port);
-console.log("App listening on port " + port);
